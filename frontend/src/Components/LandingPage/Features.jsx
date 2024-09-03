825c2dc273cc843bd354f0d069df2840c738f434
Aditi Bindal <123075271+aditi-dsi@users.noreply.github.com>
2024-09-03 23:04:22 +0530

    Update Features.jsx

--- frontend/src/Components/LandingPage/Features.jsx
+++ frontend/src/Components/LandingPage/Features.jsx
@@ -23,7 +23,7 @@ const Features = () => {
           alt="Vector Decoration"
         />
         <p className="text-lg mb-8">
-          Shwet lets you take charge of your post, news, claims or whatever you bring to the public using proof of authenctication by Reclaim Protocol.
+          Shwet lets you take charge of your post, news, claims or whatever you bring to the public with zero-knowledge proofing mechanism using Reclaim Protocol.
         </p>
         {/* <button className="bg-primary text-black font-bold py-3 px-6 rounded-full">
           Explore Now
