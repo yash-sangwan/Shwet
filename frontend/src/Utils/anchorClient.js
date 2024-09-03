c2029f762f52f9cf09a0d50641211afe8dddc317
Yash Sangwan <149103017+yash-sangwan@users.noreply.github.com>
2024-09-03 20:01:36 +0530

    Update anchorClient.js

--- frontend/src/Utils/anchorClient.js
+++ frontend/src/Utils/anchorClient.js
@@ -2,7 +2,7 @@ import { AnchorProvider, Program } from '@project-serum/anchor';
 import { Connection, PublicKey } from '@solana/web3.js';
 import idl from '../idl.json'; // Make sure to import your IDL
 
-const programID = new PublicKey('8SzEAvWdWoWyPKScZCfWRKMKTTdhCT3v1CknL7Aew9ar'); // Replace with your actual program ID
+const programID = new PublicKey('4jwLwSKQ1AM5PtfCFf6VppHdpEb3VfBAS26cbqYwU8rW'); // Replace with your actual program ID
 const network = 'https://api.devnet.solana.com'; // or your custom network
 
 export const getProgram = (wallet) => {
