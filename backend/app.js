6e72b077157ef220a1146ffe68cf107d495574b1
Aditi Bindal <123075271+aditi-dsi@users.noreply.github.com>
2024-09-03 17:51:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,23 +11,11 @@ const cors = require ("cors")
 require('dotenv').config();
 // Use CORS middleware
 app.use(cors({
-    origin: 'https://shwet.vercel.app', // Specify the allowed origin (frontend's URL)
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',  // Specify allowed HTTP methods
+    origin: '*', // Specify the allowed origin (frontend's URL)
+    methods: 'GET,POST,PUT,DELETE',  // Specify allowed HTTP methods
     allowedHeaders: 'Content-Type,Authorization',  // Specify allowed headers
 }));
 
-// Manually set headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://shwet.vercel.app');  // Manually set origin
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();  // Send OK for preflight requests
-    }
-    next();
-});
-// Handle preflight requests
-app.options('*', cors());
 
 // Connect MongoDB via mongoose
 connectDB();
