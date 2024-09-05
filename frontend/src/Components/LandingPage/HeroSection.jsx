4584c0d79cd868115e3ee5038820e5f8603859be
Yash Sangwan <149103017+yash-sangwan@users.noreply.github.com>
2024-09-05 23:50:22 +0530

    Update HeroSection.jsx

--- frontend/src/Components/LandingPage/HeroSection.jsx
+++ frontend/src/Components/LandingPage/HeroSection.jsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
   className="bg-primary text-primaryBg py-2 px-4 md:px-6 rounded font-semibold"
   onClick={() => window.open('https://youtu.be/MJnoen_rW4E', '_blank')}
 >
-  Explore
+  Watch Video
 </button>
 
 
